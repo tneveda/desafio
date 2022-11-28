1370587f798bb1942ce99b9496b724c4e75532df
tneveda <tiagoneveda@gmail.com>
2022-11-28 15:46:13 +0000

    Inseridos forms e bootstrap  p/ dados cliente

--- resources/js/components/app.vue
+++ resources/js/components/app.vue
@@ -1,37 +1,54 @@
 <template>
-    <div id="titulo">
-      
-    <h3 id="teste">Ordem de Encomenda</h3>
-    <h3 id="cenas2">Nome do Cliente</h3>
 
-    <div id="teste" class="container mx-auto mt-10 px-10">
+    <div :id="blankID">
 
-<div class="mx-auto mt-20">
-
-    <h3 class="text-lg text-center font-bold mb-4">
- <select name="transportadoraID" @change="onchange($event)" class="form-select form-control">
-  <option value="">Cenas</option>
-  <option value="DHL">DHL</option>
-  <option value="CTT">CTT</option>
-  <option value="teste">teste</option>
-  <option value="ola">ola</option>
-  <option value="Amanha">Amanha</option>
-  <option value="cara">cara</option>
-  <option value="Cenas1">Cenas1</option>
-</select>
-
-    </h3>
-
-     <div class="flex justify-center">
-
-        <Dropdown />
-
-     </div>
-
-</div>
-
-</div>
+        
+    <div :id="tituloID" class="container">
+        <div class="row">
+      <div class="col-sm-6" ><h6>Ordem de Encomenda</h6></div>
+      <div class="col-sm-6" ><h6>Encomenda</h6></div>
+       </div>
+       <br>
+       <div class="row">
+      <div :id="ClienteID" class="col-sm-3" ><h6>Nome do Cliente</h6></div>
+      <div class="col-sm-3" >
+        
+        <input v-model="nome"  placeholder="Inserir nome do Cliente">
+        <p>{{ nome }}</p>
     </div>
+       </div>
+       <div class="row">
+      <div :id="ClienteID" class="col-sm-3" ><h6>Email</h6></div>
+      <div class="col-sm-3" >
+        
+        <input v-model="email" placeholder="Inserir email do Cliente">
+        <p>{{ email }}</p>
+    </div>
+       </div>
+
+       <div class="row">
+      <div :id="ClienteID" class="col-sm-3" ><h6>Transportadora</h6></div>
+      <div class="col-sm-3" >
+        
+        <select name="transportadoraID" @change="onchange($event)" class="form-select form-control">
+  <option value="">Transportadora</option>
+  <option value="DHL">DHL</option>
+  <option value="CTT Express">CTT Express</option>
+  <option value="DPD">DPD</option>
+  <option value="Envialia">Envialia</option>
+  <option value="FedEx">FedEx</option>
+  <option value="GLS2">GLS2</option>
+  <option value="UPS">UPS</option>
+</select>
+    </div>
+       </div>
+
+
+    </div>
+    <br>
+
+    </div>
+
   
 </template>
 
@@ -47,6 +64,10 @@ components: {
 
 data() {
   return{
+    blankID:"blank",
+    tituloID:"titulo",
+    ClienteID:"nomeCliente",
+
     onchange(e){
         console.log(e.target.value);
     }
@@ -58,4 +79,23 @@ data() {
 
 <style>
 
+#blank{
+    padding-top: 150px;
+    padding-right: 350px;
+    padding-left: 350px;
+}
+
+#nomeCliente{
+    padding-left: 30px;
+}
+
+
+
+#titulo{
+    background-color: #AAAAAA;
+    padding-top: 20px;
+    padding-left: 20px;
+   
+    
+ }
 </style>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,6 +8,7 @@
 
         <!-- Fonts -->
         <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
         @vite('resources/css/app.css')
     </head>
     <body>
@@ -15,6 +16,7 @@
      <div id='app'></div>
 
      @vite('resources/js/app.js')
+     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
     
     </body>
 </html>
